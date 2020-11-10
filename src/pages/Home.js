513a47cc846c7ed7ddfa404a5145e431a7b68581
Prathamesh Mali <mali.prathamesh82@gmail.com>
2020-11-10 12:48:29 +0530

    Added new Headers in site and deployed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,20 @@
-import React from "react";
-import { IonContent, IonPage } from "@ionic/react";
-import ProductList from "../components/Product/ProductList";
-import SmallHeader from "../components/Header/SmallHeader";
-import LargeHeader from "../components/Header/LargeHeader";
+import React from 'react'
+import { IonContent, IonPage } from '@ionic/react'
+import ProductList from '../components/Product/ProductList'
+import SmallHeader from '../components/Header/SmallHeader'
+import LargeHeader from '../components/Header/LargeHeader'
 
-const Home = (props) => {
+const Home = props => {
   return (
     <IonPage>
-      <SmallHeader title="App Hunt" />
+      <SmallHeader title="Launch Pad" />
       <IonContent color="medium" fullscreen>
-        <LargeHeader title="App Hunt" />
+        <LargeHeader title="Launch Pad" />
         <br></br>
         <ProductList location={props.location} />
       </IonContent>
     </IonPage>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
